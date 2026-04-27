10fb94292ecee08dd7b68a94d51266b5b88fefcf
maxime <maxime.lefort@etik.com>
2026-04-27 15:06:38 +0200

    edit config pour que ça récupère l'ip

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,1 +1,15 @@
-export const API_BASE_URL = 'http://10.101.0.239:3000';
+import Constants from 'expo-constants';
+
+function resolveApiUrl() {
+  if (process.env.EXPO_PUBLIC_API_URL) {
+    return process.env.EXPO_PUBLIC_API_URL;
+  }
+  // En Expo Go sur device physique, hostUri contient l'IP de la machine hôte (ex: "192.168.1.10:8081")
+  const host = Constants.expoConfig?.hostUri?.split(':')[0];
+  if (host && host !== 'localhost') {
+    return `http://${host}:3000`;
+  }
+  return 'http://localhost:3000';
+}
+
+export const API_BASE_URL = resolveApiUrl();
